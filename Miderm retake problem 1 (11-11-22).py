efc08fa5e909532daba848d6f49cca009ff5876a
Jesse <114521818+Tripplejdog@users.noreply.github.com>
2022-11-11 13:20:24 -0700

    Add files via upload

---
+++ Miderm retake problem 1 (11-11-22).py
@@ -0,0 +1,12 @@
+age = int(input("please enter your age: "))
+
+if age <= 20:
+    print("you get kids bop")
+elif age <= 40:
+    print("you get to listen to brittany spears")
+elif age <= 60:
+    print("you get to rock out to some Smashing Pumpkins")
+elif age <= 80:
+    print("i'll play the Rolling Stones")
+else:
+    print("ill put on some jazz")
