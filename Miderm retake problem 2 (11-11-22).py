def6584b224f103f6acfcdada2c60a11aff264ce
Jesse <114521818+Tripplejdog@users.noreply.github.com>
2022-11-11 13:26:44 -0700

    Add files via upload

---
+++ Miderm retake problem 2 (11-11-22).py
@@ -0,0 +1,12 @@
+season = input("please enter your favorite season: ")
+
+if season == "spring":
+    print("april showers bring may flowers")
+elif season == "summer":
+    print("life's a beach")
+elif season == "fall":
+    print("get yourself a pumpkin spice latte")
+elif season == "winter":
+    print("do you want to build a snowman")
+else:
+    print(season,"is not a valid season")
