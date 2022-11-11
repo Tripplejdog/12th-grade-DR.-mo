cb3460cf60a33daa2e9e6df25b3f5fa7e279a94b
Jesse <114521818+Tripplejdog@users.noreply.github.com>
2022-11-11 14:16:08 -0700

    Add files via upload

---
+++ Miderm retake problem 5 (11-11-22).py
@@ -0,0 +1,11 @@
+doExit = False
+
+#---------------------------Warranty Function--------------------------
+def Warranty():
+    phone = "A"
+    while phone != "hello":
+        phone = input("ring ring")
+    print("extended warranty")   
+     
+
+Warranty()#function call
